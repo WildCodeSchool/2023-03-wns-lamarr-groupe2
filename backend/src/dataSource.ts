50ec585882baaae7b33d229233017ded76c04bde
Kobgones <morgan.mezaache.pro@gmail.com>
2023-07-06 12:40:42 +0200

    Added .env file location in docker-compose.yml

--- backend/src/dataSource.ts
+++ backend/src/dataSource.ts
@@ -4,31 +4,29 @@ import { join } from "path";
 
 require("dotenv").config();
 const path = join(__dirname, "./models/*.ts");
-
-const DBport =
-  process.env.DB_PORT != null ? parseInt(process.env.DB_PORT, 10) : undefined;
+const DBport = process.env.DB_PORT != null ? parseInt(process.env.DB_PORT, 10) : undefined;
 
 // We check if DB_PORT is a number
 if (typeof DBport === "number" && isNaN(DBport)) {
-  // If conversion failed, exit
-  console.error(`Invalid DB_PORT: ${DBport}`);
-  process.exit();
+	// If conversion failed, exit
+	console.error(`Invalid DB_PORT: ${DBport}`);
+	process.exit();
 }
 
 // Create a new DataSource with the following configuration :
 const dataSource = new DataSource({
-  // if we run backend without docker, the host has to be 'localhost'
-  // if we run backend with docker, the host has to be 'postgres'
-  type: process.env.DB_TYPE as any,
-  host: process.env.DB_HOST,
-  port: DBport,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD as string,
-  database: process.env.DB_NAME,
+	// if we run backend without docker, the host has to be 'localhost'
+	// if we run backend with docker, the host has to be 'postgres'
+	type: process.env.DB_TYPE as any,
+	host: process.env.DB_HOST,
+	port: DBport,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD as string,
+	database: process.env.DB_NAME,
 
-  synchronize: true,
+	synchronize: true,
 
-  entities: [path],
+	entities: [path],
 });
 
 export default dataSource;
