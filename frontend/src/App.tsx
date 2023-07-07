de51cbd22e06854b355c31053518c34507d3fba2
carlo <marion.carl498@gmail.com>
2023-07-07 15:42:38 +0100

    toggle remove package-lock

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,29 +9,32 @@ import ChallengePage from "./pages/challenge/ChallengePage";
 import CreateChallengePage from "./pages/creation-challenge/CreateChallengePage";
 import CompanyGroupsPage from "./pages/company/company-groups/CompanyGroupsPage";
 import CompanyDashboardPage from "./pages/company/company-dashboard/CompanyDashboardPage";
-
+import Toggle from "./components/Toggle";
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/scores" element={<ScoresPage />} />
-      <Route path="/challenges" element={<ChallengesPage />} />
-      <Route path="/challenges/creation" element={<CreateChallengePage />} />
-      <Route path="/challenges/:id" element={<ChallengePage />} />
-      <Route path="/settings" element={<SettingsPage />} />
-      <Route path="/notifications   " element={<NotificationsPage />} />
-      {/* Admin routes for company accounts */}
-      <Route path="/admin/dashboard" element={<CompanyDashboardPage />} />
-      <Route path="/admin/challenges" element={<ChallengesPage />} />
-      <Route path="/admin/challenges/:id" element={<ChallengePage />} />
-      <Route path="/admin/scores" element={<ScoresPage />} />
-      <Route path="/admin/teams" element={<CompanyGroupsPage />} />
-      <Route
-        path="/admin/challenges/creation"
-        element={<CreateChallengePage />}
-      />
-      <Route path="/admin/settings" element={<SettingsPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/scores" element={<ScoresPage />} />
+        <Route path="/challenges" element={<ChallengesPage />} />
+        <Route path="/challenges/creation" element={<CreateChallengePage />} />
+        <Route path="/challenges/:id" element={<ChallengePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/notifications   " element={<NotificationsPage />} />
+        {/* Admin routes for company accounts */}
+        <Route path="/admin/dashboard" element={<CompanyDashboardPage />} />
+        <Route path="/admin/challenges" element={<ChallengesPage />} />
+        <Route path="/admin/challenges/:id" element={<ChallengePage />} />
+        <Route path="/admin/scores" element={<ScoresPage />} />
+        <Route path="/admin/teams" element={<CompanyGroupsPage />} />
+        <Route
+          path="/admin/challenges/creation"
+          element={<CreateChallengePage />}
+        />
+        <Route path="/admin/settings" element={<SettingsPage />} />
+      </Routes>
+      <Toggle onClick={() => console.log("toggled")} styled={"toggle"} />
+    </>
   );
 }
