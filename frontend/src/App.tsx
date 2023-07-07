879a28783f91a7c88cfeebaa416e603fa9c183bf
carlo <marion.carl498@gmail.com>
2023-07-07 09:09:11 +0100

    cleaned app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,4 @@
 import BtnCustom from "./components/BtnCustom";
 export default function App() {
-  return (
-    <>
-      <h1 className="text-primary-good  font-titles">Echoes of Future</h1>
-      <p className="underline">hey</p>
-    </>
-  );
+  return <></>;
 }
