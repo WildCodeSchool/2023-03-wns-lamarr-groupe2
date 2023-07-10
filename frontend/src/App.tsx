0e8146a080a20ccd8743edab7ea5dcb9b5634a3e
Marion Lalonde <lalondemarion.perso@gmail.com>
2023-07-10 16:54:47 +0200

    Making change admin for company

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,29 +9,33 @@ import ChallengePage from "./pages/challenge/ChallengePage";
 import CreateChallengePage from "./pages/creation-challenge/CreateChallengePage";
 import CompanyGroupsPage from "./pages/company/company-groups/CompanyGroupsPage";
 import CompanyDashboardPage from "./pages/company/company-dashboard/CompanyDashboardPage";
+import ContextProvider from "./features/contexts/AppProvider";
+
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/scores" element={<ScoresPage />} />
-      <Route path="/challenges" element={<ChallengesPage />} />
-      <Route path="/challenges/creation" element={<CreateChallengePage />} />
-      <Route path="/challenges/:id" element={<ChallengePage />} />
-      <Route path="/settings" element={<SettingsPage />} />
-      <Route path="/notifications   " element={<NotificationsPage />} />
-      {/* Admin routes for company accounts */}
-      <Route path="/admin/dashboard" element={<CompanyDashboardPage />} />
-      <Route path="/admin/challenges" element={<ChallengesPage />} />
-      <Route path="/admin/challenges/:id" element={<ChallengePage />} />
-      <Route path="/admin/scores" element={<ScoresPage />} />
-      <Route path="/admin/teams" element={<CompanyGroupsPage />} />
-      <Route
-        path="/admin/challenges/creation"
-        element={<CreateChallengePage />}
-      />
-      <Route path="/admin/settings" element={<SettingsPage />} />
-    </Routes>
+    <ContextProvider >
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/scores" element={<ScoresPage />} />
+        <Route path="/challenges" element={<ChallengesPage />} />
+        <Route path="/challenges/creation" element={<CreateChallengePage />} />
+        <Route path="/challenges/:id" element={<ChallengePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/notifications   " element={<NotificationsPage />} />
+        {/* Admin routes for company accounts */}
+        <Route path="/company/dashboard" element={<CompanyDashboardPage />} />
+        <Route path="/company/challenges" element={<ChallengesPage />} />
+        <Route path="/company/challenges/:id" element={<ChallengePage />} />
+        <Route path="/company/scores" element={<ScoresPage />} />
+        <Route path="/company/teams" element={<CompanyGroupsPage />} />
+        <Route
+          path="/company/challenges/creation"
+          element={<CreateChallengePage />}
+        />
+        <Route path="/company/settings" element={<SettingsPage />} />
+      </Routes>
+    </ContextProvider>
   );
 }
