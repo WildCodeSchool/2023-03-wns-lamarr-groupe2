b56c26b9067b78563a6b16de1449673d80cc8e4b
Marion Lalonde <lalondemarion.perso@gmail.com>
2023-07-10 11:03:54 +0200

    Adding AdminprogressionBar

--- frontend/src/components/ProgressionBar.tsx
+++ frontend/src/components/ProgressionBar.tsx
@@ -7,12 +7,12 @@ type ProgressionBarProps = {
 
 const colorIndicator = (progression: number) => {
     if (progression >= 0 && progression <= 32) {
-        return 'bg-primary-danger';
+        return 'danger';
     }
     if (progression >= 33 && progression <= 66) {
-        return 'bg-primary-attention';
+        return 'attention';
     }
-    return 'bg-primary-good';
+    return 'good';
 };
 
 export const ProgressionBar: React.FC<ProgressionBarProps> = ({ size, value }) => {
@@ -20,9 +20,62 @@ export const ProgressionBar: React.FC<ProgressionBarProps> = ({ size, value }) =
 
     return (
         <div className={`relative h-4 max-w-[119px] ${!size && 'md:h-8 md:max-w-[275px]'} bg-white dark:bg-white rounded-r-rounder border border-primary-dark drop-shadow-progressbar`}>
-            <div className={`h-[14px] max-w-[119px] ${!size && 'md:h-[30px] md:max-w-[275px]'} ${colorClass} pr-2`} style={{ width: `${value}%` }}>
+            <div className={`h-[14px] max-w-[119px] ${!size && 'md:h-[30px] md:max-w-[275px]'} bg-primary-${colorClass} pr-2`} style={{ width: `${value}%` }}>
                 <p className={`absolute right-3 text-xs font-bold ${!size && 'md:text-xl'}`}>{value}%</p>
             </div>
         </div>
     );
 };
+
+
+export const ProgressionAdminBar: React.FC<ProgressionBarProps> = ({ size, value }) => {
+    const colorClass = colorIndicator(value);
+
+    const strokeWidth = 8
+    const radius = 40;
+    const circumference = 2 * Math.PI * radius;
+    const progress = (value / 100) * circumference;
+    const remaining = circumference - progress;
+
+    return (
+        <div className="flex justify-center items-center h-screen ">
+
+            <svg className="w-36 h-36 " viewBox={`0 0 100 100`}>
+
+                <circle
+                    className="stroke-main-grey  drop-shadow-progressbar"
+                    cx="50"
+                    cy="50"
+                    r={radius}
+                    strokeWidth={strokeWidth}
+                    strokeOpacity={1}
+                    fill="transparent"
+                    strokeDasharray={circumference}
+                />
+                <circle
+                    className={`stroke-primary-${colorClass}`}
+                    cx="50"
+                    cy="50"
+                    r={radius}
+                    strokeWidth={strokeWidth}
+                    fill="transparent"
+                    strokeDasharray={circumference}
+                    strokeDashoffset={remaining}
+                    strokeLinecap='round'
+
+                />
+                <text
+                    className="text-center text-sm"
+                    x="52"
+                    y="52"
+                    dominantBaseline="middle"
+                    textAnchor="middle"
+
+                >
+                    {value}%
+                </text>
+            </svg>
+        </div>
+    );
+};
+
