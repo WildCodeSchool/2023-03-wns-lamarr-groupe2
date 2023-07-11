2d2da5d30b73a5b6e1ad101a467d4a667ccb3fcf
Marion Lalonde <lalondemarion.perso@gmail.com>
2023-07-11 10:30:45 +0200

    Connexion Page styled

--- frontend/src/pages/homepage/Connexion/ConnexionPage.tsx
+++ frontend/src/pages/homepage/Connexion/ConnexionPage.tsx
@@ -1,6 +1,8 @@
 import { FC, PropsWithChildren, useState } from "react";
 import InputCustom from "../../../components/InputCustom";
 import { UserInformations } from "../Inscription/InscriptionPage";
+import BtnCustom from "../../../components/BtnCustom";
+import { Link } from "react-router-dom";
 
 const ConnexionPage: FC<PropsWithChildren> = () => {
 
@@ -21,21 +23,35 @@ const ConnexionPage: FC<PropsWithChildren> = () => {
     const { email, password } = userInformations;
 
     return (
-        <div>
-            <InputCustom
-                label="Email"
-                type="email"
-                name='email'
-                value={email ?? ''}
-                onChange={handleInputChange('email')}
-            />
-            <InputCustom
-                label="Mot de passe"
-                type="password"
-                name='password'
-                value={password ?? ''}
-                onChange={handleInputChange('password')}
-            />
+        <div className="relative lg:w-5/12 w-full h-screen items-center flex justify-center">
+            <div className="max-w-[276px]  w-full flex flex-col">
+                <h2 className="text-center font-titles font-bold text-2xl mb-7">S'IDENTIFIER</h2>
+                <form>
+                    <InputCustom
+                        label="Email"
+                        type="email"
+                        name='email'
+                        value={email ?? ''}
+                        onChange={handleInputChange('email')}
+                    />
+                    <InputCustom
+                        label="Mot de passe"
+                        type="password"
+                        name='password'
+                        value={password ?? ''}
+                        onChange={handleInputChange('password')}
+                    />
+                    <div className=" my-9 flex justify-center">
+                        <BtnCustom text="CONNEXION" onClick={() => console.log('connexion')} styled="btnGood" />
+                    </div>
+                </form>
+                <div className="text-tertiary-dark  text-xl font-content text-center gap-3">
+                    <p>Mot de passe oublié ?</p>
+                    <Link to='/register' className="underline">S'inscrire</Link>
+                </div>
+
+            </div>
+            <p className="absolute bottom-8 text-tertiary-dark text-sm">©2023, Echoes of Future. Tous droits réservés</p>
         </div>
     )
 }
