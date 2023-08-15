d8d702e9b872657c95f3c413571c497a491c8bc4
Hysilie <lalondemarion.perso@gmail.com>
2023-08-15 09:41:21 +0200

    Refactoring of creation challenge

--- frontend/src/pages/creation-challenge/CreateChallengePage.tsx
+++ frontend/src/pages/creation-challenge/CreateChallengePage.tsx
@@ -1,4 +1,4 @@
-import { FC, PropsWithChildren, SetStateAction, useEffect, useState } from "react";
+import { FC, PropsWithChildren, useEffect, useReducer } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import InputCustom from "../../components/InputCustom";
@@ -6,71 +6,59 @@ import DatePickers from "./DatePickers";
 import { isEmpty } from "remeda";
 import useUserContext from "../../features/contexts/UserContext";
 import TaskToDo from "./TaskToDo";
-import { OptionType } from "./DropDownSelectors";
 import plus from '../../assets/icons/plus-task.svg'
 import Toggle from "../../components/Toggle";
 import Contenders, { TContender } from "./Contenders";
 import Tags, { TTags } from "./Tags";
 import BtnCustom from "../../components/BtnCustom";
+import { initialState, reducer } from "./reducer";
+import { OptionType } from "./DropDownSelectors";
+
 
 const CreateChallengePage: FC<PropsWithChildren> = () => {
   const { user } = useUserContext()
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState("");
-  const [startDate, setStartDate] = useState<Date | null>(null);
-  const [endDate, setEndDate] = useState<Date | null>(null);
-  const [isDisabled, setIsDisabled] = useState(false);
-  const [isDisabledContenders, setIsDisabledContenders] = useState(false)
-  const [tasksToDo, setTasksToDo] = useState<any>([{}])
-  const [isPublicMode, setIsPublicMode] = useState(false)
-  const [selectedContenders, setSelectedContenders] = useState<TContender[]>([])
-  const [selectedTags, setSelectedTags] = useState<TTags[]>([])
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  /*   console.log('Title :', title,
-      '\nDescription :', description,
-      '\nStartDate :', startDate,
-      '\nEndDate :', endDate,
-      '\nTasks to do :', tasksToDo,
-      '\nIs public :', isPublicMode,
-      '\nContenders :', selectedContenders,
-      '\nTags :', selectedTags) */
+  console.log('Title :', state.title,
+    '\nDescription :', state.description,
+    '\nStartDate :', state.startDate,
+    '\nEndDate :', state.endDate,
+    '\nTasks to do :', state.tasksToDo,
+    '\nIs public :', state.isPublicMode,
+    '\nContenders :', state.selectedContenders,
+    '\nTags :', state.selectedTags)
 
-  const handleDescriptionChange = (value: SetStateAction<string>) => {
-    setDescription(value);
-  };
+
   useEffect(() => {
+    const { description, title, startDate, endDate } = state;
     if (isEmpty(description) || isEmpty(title) || startDate === null || endDate === null) {
-      setIsDisabled(true);
+      dispatch({ type: "SET_IS_DISABLED", payload: true });
     } else {
-      setIsDisabled(false);
+      dispatch({ type: "SET_IS_DISABLED", payload: false });
     }
-  }, [title, description, startDate, endDate]);
+  }, [state.description, state.title, state.startDate, state.endDate]);
 
   useEffect(() => {
-    if ((tasksToDo[0].id === undefined) || isDisabled) {
-      setIsDisabledContenders(true);
+    if ((state.tasksToDo[0].id === undefined) || state.isDisabled) {
+      dispatch({ type: "SET_IS_DISABLED_CONTENDERS", payload: true });
     } else {
-      setIsDisabledContenders(false);
+      dispatch({ type: "SET_IS_DISABLED_CONTENDERS", payload: false });
     }
-  }, [tasksToDo, isDisabled]);
+  }, [state.tasksToDo, state.isDisabled]);
 
-  /* To add a new task */
   const handleTaskList = (newTaskData?: OptionType) => {
-    if (tasksToDo.length > 2 && !user.company_id) {
+    if (state.tasksToDo.length > 2 && !user.company_id) {
       return;
     }
-    if (tasksToDo.length > 4) {
+    if (state.tasksToDo.length > 4) {
       return;
     }
-    /* Add a new task without value because it was more easy to update later */
-    setTasksToDo([...tasksToDo, {}]);
-  }
+    dispatch({ type: "ADD_TASK" });
+  };
 
-  /* Update a task with his index */
-  const updateTask = (index: number, updatedTask: OptionType) => {
-    const updatedTasks = [...tasksToDo];
-    updatedTasks[index] = updatedTask;
-    setTasksToDo(updatedTasks);
+  const updateTask = (index: number, updatedTask: OptionType | undefined) => {
+    // @ts-ignore - I don't know how to fix the payload ts error
+    dispatch({ type: "UPDATE_TASK", payload: { index: index, task: updatedTask as OptionType } });
   };
 
   return (
@@ -80,9 +68,9 @@ const CreateChallengePage: FC<PropsWithChildren> = () => {
         <InputCustom
           type="text"
           name=""
-          value={title}
+          value={state.title}
           placeholder="Be creative ! "
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={(e) => dispatch({ type: "SET_TITLE", payload: e.target.value })}
           label="TITRE"
         />
         {/* Using the rich text editor for the description */}
@@ -91,18 +79,18 @@ const CreateChallengePage: FC<PropsWithChildren> = () => {
         </label>
         <ReactQuill
           className="mt-[-18px] max-w-[100%] "
-          value={description}
-          onChange={handleDescriptionChange}
+          value={state.description}
+          onChange={(value) => dispatch({ type: "SET_DESCRIPTION", payload: value })}
           placeholder="Be creative ! "
           preserveWhitespace
         />
 
         {/*  DATE PICKER */}
         <DatePickers
-          setStartDate={setStartDate}
-          startDate={startDate}
-          setEndDate={setEndDate}
-          endDate={endDate}
+          setStartDate={(date) => dispatch({ type: "SET_START_DATE", payload: date as Date | null })}
+          startDate={state.startDate}
+          setEndDate={(date) => dispatch({ type: "SET_END_DATE", payload: date as Date | null })}
+          endDate={state.endDate}
         />
 
         {/* Task To dDo */}
@@ -110,11 +98,11 @@ const CreateChallengePage: FC<PropsWithChildren> = () => {
           Tâches à accomplir
         </label>
 
-        {tasksToDo.map((task: any, index: number) => (
-          <TaskToDo updateTask={(updatedTask: OptionType) => updateTask(index, updatedTask)} key={index} isDisabled={isDisabled} />
+        {state.tasksToDo.map((task: any, index: number) => (
+          <TaskToDo updateTask={(updatedTask: OptionType) => updateTask(index, updatedTask)} key={index} isDisabled={state.isDisabled} />
         ))}
 
-        {(tasksToDo.length > 2 && !user.company_id) ? null : tasksToDo.length === 5 ? null : <button onClick={() => handleTaskList(undefined)} className="flex gap-2 font-content">
+        {(state.tasksToDo.length > 2 && !user.company_id) ? null : state.tasksToDo.length === 5 ? null : <button onClick={() => handleTaskList(undefined)} className="flex gap-2 font-content">
           <img src={plus} alt='Create a task' className="h-6 w-6" />
           ajouter une tâche </button>}
       </section>
@@ -124,14 +112,14 @@ const CreateChallengePage: FC<PropsWithChildren> = () => {
           VISIBILITÉ
         </label>
         <div className="flex gap-6 mt-2">
-          <Toggle value={isPublicMode} onClick={() => setIsPublicMode(prev => !prev)} styled='toggle' />
-          <p> {isPublicMode ? 'publique' : 'privée'}</p>
+          <Toggle value={state.isPublicMode} onClick={() => dispatch({ type: "SET_PUBLIC_MODE", payload: !state.isPublicMode })} styled='toggle' />
+          <p> {state.isPublicMode ? 'publique' : 'privée'}</p>
         </div>
 
         {/* Contenders */}
-        <Contenders isDisabledContenders={isDisabledContenders} selectedContenders={selectedContenders} setSelectedContenders={setSelectedContenders} />
+        <Contenders isDisabledContenders={state.isDisabledContenders} selectedContenders={state.selectedContenders} setSelectedContenders={(contenders) => dispatch({ type: "SET_SELECTED_CONTENDERS", payload: contenders as TContender[] })} />
         {/* Tags */}
-        <Tags isDisabledContenders={isDisabledContenders} setSelectedTags={setSelectedTags} selectedTags={selectedTags} />
+        <Tags isDisabledContenders={state.isDisabledContenders} setSelectedTags={(tags) => dispatch({ type: "SET_SELECTED_TAGS", payload: tags as TTags[] })} selectedTags={state.selectedTags} />
 
         <div className="my-6 md:mt-24 lg:pr-24  flex gap-6 w-full justify-center lg:justify-end">
           <button onClick={() => console.log('preview')} className="text-primary-attention uppercase">aperçu</button>
