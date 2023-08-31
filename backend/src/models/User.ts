5378c8dd238984ea095a34582fa3bd6af47a7f0f
Kobgones <morgan.mezaache.pro@gmail.com>
2023-08-31 11:22:52 +0200

    Added CommentResolver and edited Challenge, Comment, User models

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,143 +1,160 @@
 import {
-  Column,
-  Entity,
-  PrimaryGeneratedColumn,
-  OneToMany,
-  ManyToMany,
-  JoinTable,
-  BaseEntity,
-} from "typeorm";
-import { ObjectType, Field } from "type-graphql";
-import { Company } from "./Company";
-import { Challenge } from "./Challenge";
-import { CompanyGroup } from "./CompanyGroup";
-import { Notification } from "./Notification";
-import { InvitationChallenge } from "./InvitationChallenge";
-import { isNotEmpty } from "class-validator";
+	Column,
+	Entity,
+	PrimaryGeneratedColumn,
+	OneToMany,
+	ManyToMany,
+	JoinTable,
+	BaseEntity,
+} from "typeorm"
+import { ObjectType, Field } from "type-graphql"
+import { Company } from "./Company"
+import { Challenge } from "./Challenge"
+import { CompanyGroup } from "./CompanyGroup"
+import { Notification } from "./Notification"
+import { InvitationChallenge } from "./InvitationChallenge"
+import { Comment } from "./Comment"
 
 @ObjectType()
 @Entity()
 export class User extends BaseEntity {
-  @Field()
-  @PrimaryGeneratedColumn()
-  id: number;
+	@Field()
+	@PrimaryGeneratedColumn()
+	id: number
 
-  @Field()
-  @Column()
-  firstname: string;
+	@Field()
+	@Column()
+	firstname: string
 
-  @Field()
-  @Column()
-  lastname: string;
+	@Field()
+	@Column()
+	lastname: string
 
-  @Field()
-  @Column()
-  username: string;
+	@Field()
+	@Column()
+	username: string
 
-  @Field()
-  @Column({ unique: true, nullable: false })
-  email: string;
+	@Field()
+	@Column({ unique: true, nullable: false })
+	email: string
 
-  @Field()
-  @Column()
-  password: string;
+	@Field()
+	@Column()
+	password: string
 
-  @Field()
-  @Column({ default: false })
-  admin: boolean;
+	@Field()
+	@Column({ default: false })
+	admin: boolean
 
-  @Field()
-  @Column({ nullable: true, default: 0 })
-  points: number;
+	@Field()
+	@Column({ nullable: true, default: 0 })
+	points: number
 
-  @Field()
-  @Column({ nullable: false, default: "hermesG" })
-  picture: string;
+	@Field()
+	@Column({ nullable: false, default: "hermesG" })
+	picture: string
 
-  @Field()
-  @Column({
-    type: "timestamptz",
-    default: new Date(new Date().getTime() + 2 * 3600 * 1000),
-  })
-  creationDate: Date;
+	@Field()
+	@Column({
+		type: "timestamptz",
+		default: new Date(new Date().getTime() + 2 * 3600 * 1000),
+	})
+	creationDate: Date
 
-  @Field(() => Company)
-  @OneToMany(() => Company, (company) => company.id)
-  @JoinTable()
-  company?: Company[];
+	@Field(() => Company)
+	@OneToMany(() => Company, (company) => company.id)
+	@JoinTable()
+	company?: Company[]
 
-  @Field(() => [CompanyGroup])
-  @OneToMany(() => CompanyGroup, (companyGroup) => companyGroup.id)
-  @JoinTable()
-  company_group?: CompanyGroup[];
+	@Field(() => [CompanyGroup])
+	@OneToMany(() => CompanyGroup, (companyGroup) => companyGroup.id)
+	@JoinTable()
+	company_group?: CompanyGroup[]
 
-  // friend_list
-  @ManyToMany(() => User, (friendId) => friendId.id)
-  @JoinTable({
-    name: "friend_list", // table name for the junction table of this relation
-    joinColumn: {
-      name: "userId",
-      referencedColumnName: "id",
-    },
-    inverseJoinColumn: {
-      name: "friendId",
-      referencedColumnName: "id",
-    },
-  })
-  @Field(() => [User])
-  friend: User[];
+	// friend_list
+	@ManyToMany(() => User, (friendId) => friendId.id)
+	@JoinTable({
+		name: "friend_list", // table name for the junction table of this relation
+		joinColumn: {
+			name: "userId",
+			referencedColumnName: "id",
+		},
+		inverseJoinColumn: {
+			name: "friendId",
+			referencedColumnName: "id",
+		},
+	})
+	@Field(() => [User])
+	friend: User[]
 
-  // challenge_member
-  @Field(() => [Challenge])
-  @ManyToMany(() => Challenge, (challenge) => challenge.contenders)
-  challenge: Challenge[];
+	// challenge_member
+	@Field(() => [Challenge])
+	@ManyToMany(() => Challenge, (challenge) => challenge.contenders)
+	challenge: Challenge[]
 
-  // creator
-  @Field(() => Challenge)
-  @OneToMany(() => Challenge, (challengeId) => challengeId.creator)
-  createdChallenges: Challenge;
+	// creator
+	@Field(() => Challenge)
+	@OneToMany(() => Challenge, (challengeId) => challengeId.creator)
+	createdChallenges: Challenge
 
-  @Field(() => [Notification])
-  @ManyToMany(() => Notification, (notification) => notification.receivers, {
-    cascade: true,
-  })
-  @JoinTable()
-  receivedNotifications: Notification[];
+	@Field(() => [Notification])
+	@ManyToMany(
+		() => Notification,
+		(notification) => notification.receivers,
+		{
+			cascade: true,
+		}
+	)
+	@JoinTable()
+	receivedNotifications: Notification[]
 
-  @Field(() => [Notification])
-  @OneToMany(() => Notification, (notification) => notification.sender, {
-    cascade: true,
-  })
-  @JoinTable()
-  sentNotifications: Notification[];
+	@Field(() => [Comment])
+	@OneToMany(() => Comment, (comment) => comment.sender, {
+		cascade: true,
+	})
+	comments: Comment[]
 
-  @Field(() => InvitationChallenge)
-  @ManyToMany(() => InvitationChallenge, (invitation) => invitation.receivers, {
-    cascade: true,
-  })
-  @JoinTable({
-    name: "received_invitation", // table name for the junction table of this relation
-    joinColumn: {
-      name: "userId",
-      referencedColumnName: "id",
-    },
-    inverseJoinColumn: {
-      name: "challenge_invitation",
-      referencedColumnName: "id",
-    },
-  })
-  receivedChallengeInvitation: InvitationChallenge[];
+	@Field(() => [Notification])
+	@OneToMany(() => Notification, (notification) => notification.sender, {
+		cascade: true,
+	})
+	@JoinTable()
+	sentNotifications: Notification[]
 
-  async addFriend(friend: User): Promise<void> {
-    if (!this.friend) {
-      this.friend = [];
-    }
+	@Field(() => InvitationChallenge)
+	@ManyToMany(
+		() => InvitationChallenge,
+		(invitation) => invitation.receivers,
+		{
+			cascade: true,
+		}
+	)
+	@JoinTable({
+		name: "received_invitation", // table name for the junction table of this relation
+		joinColumn: {
+			name: "userId",
+			referencedColumnName: "id",
+		},
+		inverseJoinColumn: {
+			name: "challenge_invitation",
+			referencedColumnName: "id",
+		},
+	})
+	receivedChallengeInvitation: InvitationChallenge[]
 
-    if (
-      !this.friend.some((existingFriend) => existingFriend.id === friend.id)
-    ) {
-      this.friend.push(friend);
-      await this.save();
-    }
-  }
+	async addFriend(friend: User): Promise<void> {
+		if (!this.friend) {
+			this.friend = []
+		}
+
+		if (
+			!this.friend.some(
+				(existingFriend) =>
+					existingFriend.id === friend.id
+			)
+		) {
+			this.friend.push(friend)
+			await this.save()
+		}
+	}
 }
---
+++ backend/src/resolvers/CommentResolver.ts
@@ -0,0 +1,94 @@
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql"
+// import { Challenge } from "../models/Challenge"
+import { Comment } from "../models/Comment"
+import { User } from "../models/User"
+
+@Resolver()
+export class CommentResolver {
+	// get all comments of a user
+	@Query(() => [Comment])
+	async getCommentsOfUser(
+		@Arg("userId") userId: number
+	): Promise<Comment[]> {
+		const comments = await Comment.find({
+			relations: ["sender", "challenge_id"],
+			where: {
+				sender: {
+					id: userId,
+				},
+			},
+		})
+		return comments
+	}
+
+	// get all comments of a challenge
+	@Query(() => [Comment])
+	async getCommentsOfChallenge(
+		@Arg("challengeId") challengeId: number
+	): Promise<Comment[]> {
+		const comments = await Comment.find({
+			relations: ["sender", "challenge_id"],
+			where: {
+				challenge_id: {
+					id: challengeId,
+				},
+			},
+		})
+		return comments
+	}
+
+	// get all comments
+	@Query(() => [Comment])
+	async getAllComments(): Promise<Comment[]> {
+		const comments = await Comment.find({
+			relations: ["sender", "challenge_id"],
+		})
+		return comments
+	}
+
+	// create a comment
+	// TO FIX: challenge_id is not being saved
+	@Mutation(() => Comment)
+	async createComment(
+		@Ctx() userContext: { user: User },
+		// @Ctx() challengeContext: { challenge: Challenge },
+		@Arg("content") content: string
+	): Promise<Comment> {
+		if (!userContext.user)
+			throw new Error(
+				"You have to be authenticated to post a comment!"
+			)
+
+		const comment = Comment.create({
+			content,
+			sender: userContext.user,
+			// challenge_id: [challengeContext.challenge],
+		})
+
+		await comment.save()
+
+		return comment
+	}
+
+	// update a comment
+	@Mutation(() => Comment)
+	async updateComment(
+		@Arg("id") id: number,
+		@Arg("content") content: string
+	): Promise<Comment> {
+		const comment = await Comment.findOne({ where: { id } })
+		if (!comment) throw new Error("Comment not found!")
+
+		comment.content = content
+		await comment.save()
+
+		return comment
+	}
+
+	// delete a comment
+	@Mutation(() => Boolean)
+	async deleteComment(@Arg("id") id: number): Promise<boolean> {
+		await Comment.delete(id)
+		return true
+	}
+}
