18f7cbd94cd12c493995f77078cf05fbeb95a1f0
Hysilie <lalondemarion.perso@gmail.com>
2023-10-02 14:15:44 +0200

    Prettier and lint

--- frontend/src/components/HeaderBar.tsx
+++ frontend/src/components/HeaderBar.tsx
@@ -23,8 +23,9 @@ export const HeaderBar = () => {
     value: string | number;
   }) => (
     <div
-      className={`${label === "Challenges : " && "hidden"
-        } lg:flex items-center`}
+      className={`${
+        label === "Challenges : " && "hidden"
+      } lg:flex items-center`}
     >
       <p className="inline-block">{label}</p>
       <span className="font-bold text-[1.1em] pl-1">{value}</span>
@@ -33,8 +34,9 @@ export const HeaderBar = () => {
 
   return (
     <header
-      className={`relative  border-b-1 border-tertiary-dark  max-h-[4.5rem] ${mobileHeader ? "flex  h-20 px-8  " : "hidden"
-        }  lg:flex font-titles justify-between items-center lg:px-2`}
+      className={`relative  border-b-1 border-tertiary-dark  max-h-[4.5rem] ${
+        mobileHeader ? "flex  h-20 px-8  " : "hidden"
+      }  lg:flex font-titles justify-between items-center lg:px-2`}
     >
       <h1 className="hidden lg:block font-bold text-[3rem]">
         ECHOES OF FUTURE
